c6f30da5453e67e4bcc40f05558d38d5e19ac6fa
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-01-16 08:37:21 +0100

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float maxSpeed = 7;
+    [SerializeField] private float maxSpeed = 7f;
+    [SerializeField] private float jumpHeight = 150f;
+
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private LayerMask groundLayer;
+
     private Animator animator;
     private Rigidbody2D rigidbody2d;
     private bool isFacingRight;
+    private bool isGrounded;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
         isFacingRight = true;
+        isGrounded = false;
     }
+
+    private void Update()
+    {
+        if (isGrounded & Input.GetAxis("Jump") > 0)
+        {
+            isGrounded = false;
+            animator.SetBool("isGrounded", false);
+            rigidbody2d.AddForce(new(x:0, y:jumpHeight));
+        }
+    }
+
     private void FixedUpdate()
     {
         float move = Input.GetAxis("Horizontal");
@@ -24,6 +42,10 @@ public class PlayerController : MonoBehaviour
             x: move * maxSpeed,
             y: rigidbody2d.velocity.y);
         if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight)) Flip();
+
+        isGrounded = Physics2D.OverlapCircle(groundChecker.position, .15f, groundLayer);
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("verticalSpeed", rigidbody2d.velocity.y);
     }
     private void Flip()
     {
