79bb09b219c30c6f8f3d9a54b9640a66028d356a
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-03-05 08:48:32 +0100

    add working player healthbar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private GameObject bloodDrops;
     [SerializeField] private float maxHealth = 10f;
+    [SerializeField] private Slider healthBar;
     private float currentHealt;
 
     private void Start()
     {
         currentHealt = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
         currentHealt -= damage;
+        healthBar.value = currentHealt;
         Instantiate(bloodDrops, transform.position, transform.rotation);
 
         if (currentHealt <= 0)
