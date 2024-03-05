00b3a170b917d6f8c3d558cecae29189b61b93da
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-03-05 12:14:54 +0100

    add player health UI indicators

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,18 +6,37 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private GameObject bloodDrops;
     [SerializeField] private float maxHealth = 10f;
     [SerializeField] private Slider healthBar;
+    [SerializeField] private Image bloodScreenFX;
+    [SerializeField] private float fadeSmooth = 2f;
+
     private float currentHealt;
+    private bool getDamage;
 
     private void Start()
     {
+        getDamage = false;
         currentHealt = maxHealth;
         healthBar.maxValue = maxHealth;
         healthBar.value = maxHealth;
     }
 
+    private void Update()
+    {
+        if (getDamage) bloodScreenFX.color = new(255, 255, 255, .8f);
+        else
+        {
+            bloodScreenFX.color = Color.Lerp(
+                bloodScreenFX.color,
+                new(255, 255, 255, 0f),
+                Time.deltaTime * fadeSmooth);
+        }
+        getDamage = false;
+    }
+
     public void TakeDamage(float damage)
     {
         currentHealt -= damage;
+        getDamage = true;
         healthBar.value = currentHealt;
         Instantiate(bloodDrops, transform.position, transform.rotation);
 
@@ -30,5 +49,6 @@ public class PlayerHealth : MonoBehaviour
     private void MakeDead()
     {
         Destroy(gameObject);
+        bloodScreenFX.color = new(255, 255, 255, 1f);
     }
 }
