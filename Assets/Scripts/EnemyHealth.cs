043d641407a2189225633de14b0e15f92354a4ea
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-03-19 08:37:12 +0100

    floatint hp bar

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private float maximumHealth = 5f;
     [SerializeField] private GameObject destroyFX;
+    [SerializeField] private Slider healthBar;
+
     private float currentHealth;
 
     private void Start()
     {
         currentHealth = maximumHealth;
+        healthBar.maxValue = maximumHealth;
+        healthBar.value = currentHealth;
     }
 
     public void AddDamage(float damage)
     {
         currentHealth -= damage;
+        if (!healthBar.IsActive()) healthBar.gameObject.SetActive(true);
+        healthBar.value = currentHealth;
         if (currentHealth <= 0) MakeDead();
     }
 
