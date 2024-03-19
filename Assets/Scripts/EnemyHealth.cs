e45dcd27b2975eb1e59347a1928d489341d2c4e9
szilvasikornel <78079529+szilvasikornel@users.noreply.github.com>
2024-03-19 11:39:22 +0100

    asd

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private float maximumHealth = 5f;
     [SerializeField] private GameObject destroyFX;
+    [SerializeField] private Slider healthBar;
     private float currentHealth;
 
     private void Start()
     {
         currentHealth = maximumHealth;
+        healthBar.value = currentHealth;
     }
 
     public void AddDamage(float damage)
